872995172f7fb16306572c9311140ff6a617d5b0
Tres Tarwater <trest03@gmail.com>
2020-05-27 20:57:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0 && number % 5 == 0
+  if int % 3 == 0 && int % 5 == 0
     puts "FizzBuzz"
-  elsif number % 3 == 0
+  elsif int % 3 == 0
     puts "Fizz"
-  elsif number % 5 == 0 
+  elsif int % 5 == 0 
     puts "Buzz"
   else 
     puts nil
