bcaa091755a4d1884766a9b05b30c7a346f9512f
Tres Tarwater <trest03@gmail.com>
2020-05-27 21:17:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
+   if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3 and 5 
+   "FizzBuzz"
+  elsif int % 3 == 0 # if the number int is divisible by 3
    "Fizz" 
   elsif int % 5 == 0 # if the number int is divisible by 5
   "Buzz" 
-  elsif int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3 and 5 
-   "FizzBuzz"
   else 
     puts nil
 end
