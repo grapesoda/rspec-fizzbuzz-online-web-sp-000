e2402c44301eed46c3316462c91d850e76b1d16e
Tres Tarwater <trest03@gmail.com>
2020-05-27 21:10:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
-  elsif int % 5 == 0 
-    puts "Buzz"
-    elsif int % 3 == 0 && int % 5 == 0
+    "Fizz" 
+  elsif int % 5 == 0 # if the number int is divisible by 5
+    "Buzz" 
+    puts "Buzz" 
+    elsif int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3 and 5 "FizzBuzz" 
     puts "FizzBuzz"
   else 
     puts nil
